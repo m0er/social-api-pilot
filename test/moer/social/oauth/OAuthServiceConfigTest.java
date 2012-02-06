3f031bc96724fef2a3aebbcfa420238dfbe110d0
Yunseok Choi <ethdemor@gmail.com>
2012-02-06 22:58:15 +0900

    String to Class 타입 변환 테스트.

--- test/moer/social/oauth/OAuthServiceConfigTest.java
+++ test/moer/social/oauth/OAuthServiceConfigTest.java
@@ -8,6 +8,8 @@ import moer.social.oauth.OAuthServiceConfig;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.scribe.builder.api.*;
+import org.scribe.builder.api.TwitterApi.Authenticate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,15 @@ public class OAuthServiceConfigTest {
 		logger.info(twitterOAuthServiceConfig.getApiKey());
 		logger.info(twitterOAuthServiceConfig.getApiSecret());
 		logger.info(twitterOAuthServiceConfig.getApiKey());
+	}
+	
+	@Test
+	public void typeConversion() throws Exception {
+		logger.info(Authenticate.class.getName());
+		@SuppressWarnings("unchecked")
+		Class<Api> cls = (Class<Api>) Class.forName("org.scribe.builder.api.TwitterApi$Authenticate");
+		twitterOAuthServiceConfig.setApiClass(cls);
+		assertThat(twitterOAuthServiceConfig.getApiClass().getName(), is("org.scribe.builder.api.TwitterApi$Authenticate"));
 	}
 
 }
